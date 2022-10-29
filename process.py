99a2ae10976336e165434e52877965f3f25e31d5
Fanjin Zhang <zfjsail@163.com>
2022-10-29 17:26:37 +0800

    regressor

--- process.py
+++ process.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import csv
+import numpy as np
 from collections import defaultdict as dd
 from tqdm import tqdm
 
@@ -63,5 +66,174 @@ def gen_test_author_per_year_citation_30(pred_year=2016):
             wf.write(author + "," + ",".join([str(author_to_year_to_citation[author][i]) for i in range(1982, 2012)]) + "\n")
 
 
+def feature_extraction(pred_year=2016):
+    data_author = pd.read_csv("data/{}/raw/author.txt".format(pred_year),
+                              header=None, sep='\t', encoding='utf8').values
+    data_paper = pd.read_csv("data/{}/raw/paper.txt".format(pred_year), header=None,
+                             sep='\t', encoding='utf8').values
+    data_citation_train = pd.read_csv(
+        "data/{}/raw/citation_train.txt".format(pred_year), header=None, sep='\t', encoding='utf8').values
+    data_citation_test = pd.read_csv(
+        "data/{}/raw/citation_test.txt".format(pred_year), header=None, sep='\t', encoding='utf8').values
+    data_citation_result = pd.read_csv(
+        "data/{}/raw/citation_result.txt".format(pred_year), header=None, sep='\t', encoding='utf8').values
+    print("Finish loading data")
+
+    citation_by_year = {}  # citation_by_year[author][year] = ?
+    citation_by_paper = {}  # citation_by_paper[author][paper] = ?
+    total_citation = {}  # total_citation[author] = ?
+    total_papers = {}  # total_papers[author] = ?
+    h_index = {}  # h_index[author] = ?
+
+    paper_to_author = {}  # paper_to_author[paper] = {}
+    meeting_to_paper = {}  # meeting_to_paper[meeting][paper] = ?
+    h_rank = {}  # h_rank[meeting] = ?
+    p_rank = {}
+    author_rank1 = {}
+    author_rank2 = {}
+
+    for line in data_author:
+        citation_by_year[line[1]] = {}
+        for i in range(1992, 2012):
+            citation_by_year[line[1]][i] = 0
+        citation_by_paper[line[1]] = {}
+        total_papers[line[1]] = 0
+        total_citation[line[1]] = 0
+        h_index[line[1]] = 0
+        author_rank1[line[1]] = 0
+        author_rank2[line[1]] = 0
+    print("Init the features")
+
+    authors = []
+    meeting = ""
+    year = 0
+    for line in data_paper:
+        if line[0][0:2] == "#@":
+            authors = line[0][2:].split(', ')
+            for author in authors:
+                total_papers[author] += 1
+        if line[0][0:2] == "#t":
+            year = int(line[0][2:])
+        if line[0][0:2] == "#c":
+            meeting = line[0][2:]
+        if line[0][0:6] == "#index":
+            paper = line[0][6:]
+            if paper not in paper_to_author:
+                paper_to_author[paper] = authors
+            else:
+                paper_to_author[paper] = list(set(paper_to_author[paper] + authors))
+            if year > 2006:
+                if meeting not in meeting_to_paper:
+                    meeting_to_paper[meeting] = {}
+                meeting_to_paper[meeting][paper] = 0
+    print("Finish processing paper_to_author[] & total_papers[] ")
+
+    year = 0
+    for line in data_paper:
+        if line[0][0:2] == "#t":
+            year = int(line[0][2:])
+        if line[0][0:2] == "#%":
+            paper = line[0][2:]
+            if paper in paper_to_author:
+                for author in paper_to_author[paper]:
+                    if year >= 1992:
+                        citation_by_year[author][year] += 1
+                    total_citation[author] += 1
+                    if paper not in citation_by_paper[author]:
+                        citation_by_paper[author][paper] = 1
+                    else:
+                        citation_by_paper[author][paper] += 1
+    print("Finish processing citation_by_year[] & citation_by_paper[]")
+
+    true_citation = {}
+    rank_by_paper = {}
+    for line in data_citation_train:
+        true_citation[line[1]] = line[2]
+    for paper in paper_to_author:
+        authors = [author for author in paper_to_author[paper] if author in true_citation]
+        counts = [true_citation[author] for author in authors]
+        if len(authors) > 0:
+            p_rank[paper] = np.min(np.array(counts))
+            index = np.where(np.array(counts) == p_rank[paper])
+            auth = authors[index[0][0]]
+            for x in index[0]:
+                if total_citation[authors[x]] != 0:
+                    auth = authors[x]
+                    break
+            if total_citation[auth] == 0:
+                p_rank[paper] = int(p_rank[paper] / total_papers[auth])
+            elif paper in citation_by_paper[auth]:
+                p_rank[paper] = int(citation_by_paper[auth][paper] * p_rank[paper] / total_citation[auth])
+            else:
+                p_rank[paper] = 0
+            for author in paper_to_author[paper]:
+                if author not in rank_by_paper:
+                    rank_by_paper[author] = {}
+                rank_by_paper[author][paper] = p_rank[paper]
+    print("Finish processing rank_by_paper")
+
+    for line in data_author:
+        author = line[1]
+        contain = rank_by_paper.get(author)
+        if contain is None:
+            author_rank1[author] = 50
+        else:
+            author_rank1[author] = int(np.sum(np.array(list(contain.values()))))
+        papers = sorted(citation_by_paper[author].items(), key=lambda x: x[1], reverse=True)
+        for i in range(len(papers)):
+            if i + 1 > papers[i][1]:
+                h_index[author] = i
+                break
+    print("Finish processing h_index[] & author_rank1")
+
+    for meeting in meeting_to_paper:
+        for paper in meeting_to_paper[meeting]:
+            tmp = paper_to_author[paper][0]
+            if paper in citation_by_paper[tmp]:
+                meeting_to_paper[meeting][paper] = citation_by_paper[tmp][paper]
+        papers = sorted(meeting_to_paper[meeting].items(), key=lambda x: x[1], reverse=True)
+        h_rank[meeting] = 0
+        for i in range(len(papers)):
+            if i + 1 > papers[i][1]:
+                h_rank[meeting] = i
+                break
+        for paper in meeting_to_paper[meeting]:
+            for author in paper_to_author[paper]:
+                author_rank2[author] += h_rank[meeting]
+    print("Finish processing author_rank2")
+
+    with open('data/{}/processed/train.csv'.format(pred_year), 'w', newline='') as ft:
+        writer = csv.writer(ft)
+        writer.writerow(['author', 'author_rank1', 'author_rank2', 'total_citation', 'total_papers', 'h_index',
+                         '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001',
+                             '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+                             'result'])
+        for line in data_citation_train:
+            author, result = line[1], line[2]
+            data = [author, author_rank1[author], author_rank2[author], total_citation[author], total_papers[author], h_index[author]]
+            for i in range(1992, 2012):
+                data.append(citation_by_year[author][i])
+            data.append(int(result))
+            writer.writerow(data)
+    print("Finish writing train.csv")
+
+    with open('data/{}/processed/test.csv'.format(pred_year), 'w', newline='') as ft:
+        writer = csv.writer(ft)
+        writer.writerow(['author', 'author_rank1', 'author_rank2', 'total_citation', 'total_papers', 'h_index',
+                         '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001',
+                             '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+                             'result'])
+        for i in range(len(data_citation_test)):
+            author = data_citation_test[i][1]
+            result = data_citation_result[i][1]
+            data = [author, author_rank1[author], author_rank2[author], total_citation[author], total_papers[author], h_index[author]]
+            for j in range(1992, 2012):
+                data.append(citation_by_year[author][j])
+            data.append(int(result))
+            writer.writerow(data)
+    print("Finish writing test.csv")
+
+
 if __name__ == "__main__":
-    gen_test_author_per_year_citation_30(pred_year=2016)
+    # gen_test_author_per_year_citation_30(pred_year=2016)
+    feature_extraction(pred_year=2016)
---
+++ regressor.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import time
+from regex import R
+from xgboost import XGBRegressor, plot_importance, plot_tree
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.tree import DecisionTreeRegressor, ExtraTreeRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor, BaggingRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+import math
+
+
+def predict(pred_year=2016):
+    # 数据读入
+    train = pd.read_csv('data/{}/processed/train.csv'.format(pred_year))
+    test = pd.read_csv('data/{}/processed/test.csv'.format(pred_year))
+    x_train = train[['author_rank1', 'author_rank2', 'total_citation', 'total_papers', 'h_index',
+                     '1992', '1993', '1994', '1995', '1996',
+                     '1997', '1998', '1999', '2000', '2001',
+                     '2002', '2003', '2004', '2005', '2006',
+                     '2007', '2008', '2009', '2010', '2011'
+                     ]]
+    y_train = train['result']
+    x_test = test[['author_rank1', 'author_rank2', 'total_citation', 'total_papers', 'h_index',
+                   '1992', '1993', '1994', '1995', '1996',
+                   '1997', '1998', '1999', '2000', '2001',
+                   '2002', '2003', '2004', '2005', '2006',
+                   '2007', '2008', '2009', '2010', '2011'
+                   ]]
+    y_test = test['result']
+    print("All the data has been read")
+
+    # Pipeline设置：归一化与降维
+    # pipe_lr = Pipeline([('sc',StandardScaler()),
+    #                     ('pca', PCA(n_components=2)),
+    #                     ('svr', SVR())])
+    # t1 = time.perf_counter()
+    # pipe_lr.fit(x_train, y_train)
+    # t2 = time.perf_counter()
+    # print("fit time:", t2 - t1)
+    # y = pipe_lr.predict(x_test)
+    # t3 = time.perf_counter()
+    # print("Predict time:", t3 - t2)
+    # rmse = math.sqrt(mean_squared_error(y_test, y))
+    # print(rmse)
+
+    # 网格搜索
+    # regressor = XGBRegressor(eval_metric='rmse', n_jobs=-1)
+    # param_grid = {'max_depth': [x for x in range(3,11)],
+    #                 'learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+    #                 'n_estimators' : [20 * x for x in range(1, 11)]}
+    # grid_search = GridSearchCV(regressor, param_grid, scoring='neg_mean_squared_error')
+    # grid_result = grid_search.fit(x_train, y_train)
+    # print("Best: %f using %s" % (grid_result.best_score_,grid_search.best_params_))
+
+    # 模型选择
+    # DecisionTreeRegressor(max_depth=7)
+    # BaggingRegressor(base_estimator=DecisionTreeRegressor(max_depth=7), n_estimators=40)
+    # AdaboostRegressor(base_estimator=DecisionTreeRegressor(max_depth=7), learning_rate=0.1, n_estimators=150)
+    # GradientBoostingRegressor(learning_rate=0.1, n_estimators=100)
+    # XGBRegressor(max_depth=7, learning_rate=0.1, n_estimators=50, eval_metric='rmse')
+    models = ['LinearRegression', 'DecisionTreeRegressor',
+         'RandomForestRegressor', 'AdaBoostRegressor', 'GradientBoostingRegressor',
+         'BaggingRegressor']
+    for model in models:
+        print("===============================")
+        print(model)
+        regressor = eval(model)()  # 可设置模型具体参数
+        t1 = time.perf_counter()
+        regressor.fit(x_train, y_train)
+        t2 = time.perf_counter()
+        print("fit time:", t2 - t1)
+        y = regressor.predict(x_test)
+        t3 = time.perf_counter()
+        print("Predict time:", t3 - t2)
+        rmse = math.sqrt(mean_squared_error(y_test, y))
+        print("RMSE: ", rmse)
+
+    # 特征重要性评估
+    # l = ['author_rank1', 'author_rank2', 'total_citation', 'total_papers', 'h_index',
+    #              '1992', '1993', '1994', '1995', '1996',
+    #              '1997', '1998', '1999', '2000', '2001',
+    #              '2002', '2003', '2004', '2005', '2006',
+    #              '2007', '2008', '2009', '2010', '2011']
+    # print([l[x] for x in np.argsort(-regressor.feature_importances_)])
+
+    # 保存数据
+    with open("output/gbrt_output.txt", 'w', encoding='utf8') as fo:
+        with open("data/{}/raw/citation_test.txt".format(pred_year), 'r', encoding='utf8') as ft:
+            lines = ft.readlines()
+            for i in range(len(lines)):
+                words = lines[i].strip().split('\t')
+                _id = words[0]
+                fo.write(_id + '\t' + str(y[i]) + '\n')
+
+
+if __name__ == '__main__':
+    predict()
